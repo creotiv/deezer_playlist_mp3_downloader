ed8945331ef620a2ac28cb99f32c64dc3f3c1bde
ANdrey Nikishaev <creotiv@gmail.com>
2017-04-08 19:46:38 +0300

    fix

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'dzget=deezermp3:dzget:main',
+            'dzget=deezermp3:main',
         ],
     },
 )
