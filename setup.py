772e1d55375d632d4c89755929b7005188b9702b
ANdrey Nikishaev <creotiv@gmail.com>
2017-05-17 21:14:34 +0300

    youtube-dl library update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.4',
+    version='0.0.5',
 
     description='Download MP3 from YouTube to your PC using Deezer API',
     long_description=long_description,
@@ -67,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests==2.13.0', 'youtube-dl==2017.4.3'],
+    install_requires=['requests==2.13.0', 'youtube-dl==2017.5.14'],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
