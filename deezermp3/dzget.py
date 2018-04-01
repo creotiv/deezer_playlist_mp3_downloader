98674162dcf7bc598ce6a8533e76ca5cc1f99017
Bachynin Ivan <bachynin.i@gmail.com>
2018-04-01 23:49:12 +0300

    🎨 refactor

--- deezermp3/dzget.py
+++ deezermp3/dzget.py
@@ -1,25 +1,24 @@
-import requests
-import re
-import os
-import sys
-import subprocess
 import argparse
+import os
+import re
+
+import requests
 from youtube_dl import YoutubeDL
 
-API_URL = "https://api.deezer.com/playlist/" 
+
+API_URL = "https://api.deezer.com/playlist/"
+
 
 class DeezerMP3(object):
-
     def __init__(self, dirout=None, quality=5, format="best"):
         self.dirout = os.path.realpath(dirout or os.path.join(os.getcwd(), os.path.dirname(__file__)))
         self.quality = quality
         self.format = format
-        
+
     def log(self, data):
         print(data)
 
     def download_playlist(self, url):
-
         plid = url.strip().split('/')[-1]
         url = API_URL + plid
 
@@ -30,9 +29,9 @@ class DeezerMP3(object):
         dir_path = os.path.join(self.dirout, pl_name)
         if not os.path.exists(dir_path):
             os.mkdir(dir_path)
-        
-        regexp_video = re.compile(r'(\/watch\?[^\"]+)', re.I | re.M | re.U)
-        
+
+        regexp_video = re.compile(r'(/watch\?[^\"]+)', re.I | re.M | re.U)
+
         postprocessors = [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': self.format,
@@ -45,11 +44,11 @@ class DeezerMP3(object):
             'outtmpl': '%s/%%(title)s.%%(ext)s' % dir_path,
             'format': 'bestaudio/best',
         }
-        
+
         with YoutubeDL(options) as ydl:
             for item in data['tracks']['data']:
                 name = '%s - %s' % (item['artist']['name'], item['title'])
-                        
+
                 self.log("\n## Parsing: %s" % name)
                 yres = requests.get('https://m.youtube.com/results?search_query=%s' % name)
                 data = yres.content.decode('utf-8')
@@ -60,58 +59,65 @@ class DeezerMP3(object):
                 self.log(' > Url: %s' % videos[0])
                 ydl.download(["http://www.youtube.com%s" % videos[0]])
 
-def main():
+
+def get_args():
     parser = argparse.ArgumentParser(description="DeezerMP3 downloader")
     parser.add_argument('-p',
                         '--playlist',
                         action='store_true',
-                        help='Download playlist mode.')
+                        help='Download by playlist url.')
 
     parser.add_argument('-f',
                         '--audio-format',
                         metavar='format',
                         default='best',
                         type=str,
-                        help='Specify audio format: "best", "aac", "flac", "mp3", "m4a", "opus", "vorbis", or "wav" (default "best")')
-                        
+                        help='Specify audio format: "best", "aac", "flac", "mp3", '
+                             '"m4a", "opus", "vorbis", or "wav" (default "best")')
+
     parser.add_argument('-d',
                         '--dir',
                         metavar='directory',
                         default='./',
                         type=str,
                         help='Output directory')
-                        
+
     parser.add_argument('-q',
                         '--audio-quality',
                         metavar='quality',
                         default='5',
                         type=str,
-                        help='Specify ffmpeg/avconv audio quality, insert a value between 0 (better) and 9 (worse) for VBR or a specific bitrate like 128K (default 5)')
-    
+                        help='Specify ffmpeg/avconv audio quality, insert a value '
+                             'between 0 (better) and 9 (worse) for VBR or a specific '
+                             'bitrate like 128K (default 5)')
+
     parser.add_argument('url', metavar='url', type=str,
-                    help='Deezer url')
+                        help='Playlist urls or ids')
 
     args = parser.parse_args()
-    
+
     if args.audio_format:
-        if args.audio_format not in ['best', 'aac', 'flac', 'mp3', 'm4a', 'opus', 'vorbis', 'wav']:
-            parser.error('invalid audio format specified')
-            
+        formats = ['best', 'aac', 'flac', 'mp3', 'm4a', 'opus', 'vorbis', 'wav']
+        if args.audio_format not in formats:
+            formatted = '\n'.join(map(lambda x: ' - ' + x, formats))
+            parser.error('Invalid audio format specified. Should be one from list:\n%s' % formatted)
     if args.audio_quality:
         args.quality = args.audio_quality.strip('k').strip('K')
         if not args.audio_quality.isdigit():
-            parser.error('invalid audio quality specified')
-            
+            parser.error('Invalid audio quality specified.')
+    return args
+
+
+def main():
+    args = get_args()
+
     dmp3 = DeezerMP3(
         dirout=args.dir,
-        quality=args.audio_quality, 
-        format=args.audio_format
-    )
-            
-    if args.playlist:
-        dmp3.download_playlist(args.url)
-    else:
-        parser.error('invalid mode')
-        
+        quality=args.audio_quality,
+        format=args.audio_format)
+
+    dmp3.download_playlist(args.url)
+
+
 if __name__ == "__main__":
     main()
