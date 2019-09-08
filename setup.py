d10927aa2ca5b01a9a142726f3cac83ebaef4c7c
Andrey Nikishaev <creotiv@gmail.com>
2019-09-08 11:38:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests==2.20.0', 'youtube-dl==2018.09.18'],
+    install_requires=['requests==2.22.0', 'youtube-dl==2019.9.1'],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
